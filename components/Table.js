76b3d904f6852f83e7842a932b75d9ba292664c6
Aman Kumar <2019200@iiitdmj.ac.in>
2021-10-20 12:03:16 +0530

    added calander and clock

--- components/Table.js
+++ components/Table.js
@@ -168,7 +168,7 @@ const Table = () => {
 			<div className={classes.table}>
 				<table cellSpacing='0' className={classes.tb}>
 					<tr style={{ marginBottom: '1rem' }}>
-						<th style={{ display: 'flex' }}>
+						<th style={{ display: 'flex', alignItems: 'center' }}>
 							<span>
 								<input
 									type='checkbox'
@@ -225,12 +225,52 @@ const Table = () => {
 									</span>
 									<div>
 										<p>{data.id}</p>
-										<p>{data.date}</p>
-										<p>{data.time}</p>
+										<p
+											style={{
+												fontWeight: '300',
+												display: 'flex',
+												columnGap: '0.2rem',
+												alignItems: 'center',
+											}}
+										>
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												width='10'
+												height='10'
+												viewBox='0 0 24 24'
+											>
+												<path
+													d='M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z'
+													fill='#366EF1'
+												/>
+											</svg>{' '}
+											{data.date}
+										</p>
+										<p
+											style={{
+												fontWeight: '300',
+												display: 'flex',
+												alignItems: 'center',
+												columnGap: '0.2rem',
+											}}
+										>
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												width='10'
+												height='10'
+												viewBox='0 0 24 24'
+											>
+												<path
+													d='M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-7v-8h2v6h5v2z'
+													fill='#366EF1'
+												/>
+											</svg>
+											{data.time}
+										</p>
 									</div>
 								</td>
 								<td>{data.shopify}</td>
-								<td>
+								<td style={{ textAlign: 'left' }}>
 									<p
 										style={{
 											whiteSpace: 'nowrap',
@@ -243,7 +283,13 @@ const Table = () => {
 									<p>Qty : {data.qty}</p>
 								</td>
 								<td>
-									<p style={{ display: 'flex', columnGap: '0.5rem' }}>
+									<p
+										style={{
+											display: 'flex',
+											columnGap: '0.5rem',
+											textAlign: 'left',
+										}}
+									>
 										<span
 											style={{
 												whiteSpace: 'nowrap',
@@ -316,13 +362,14 @@ const Table = () => {
 										</span>
 									</p>
 								</td>
-								<td>
+								<td style={{ textAlign: 'left' }}>
 									<p>{data.cust}</p>
 									<p
 										style={{
 											whiteSpace: 'nowrap',
 											overflow: 'hidden',
 											textOverflow: 'ellipsis',
+											fontWeight: '300',
 										}}
 									>
 										{data.phNum}
@@ -332,6 +379,7 @@ const Table = () => {
 											whiteSpace: 'nowrap',
 											overflow: 'hidden',
 											textOverflow: 'ellipsis',
+											fontWeight: '300',
 										}}
 									>
 										{data.mail}
